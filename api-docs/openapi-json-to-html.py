05fc8528aaf91dbace983f4da6de42644d46250f
icaroribeiro <icaroribeiro@hotmail.com>
2020-04-24 12:26:43 -0300

    Updated the API documentation

--- api-docs/openapi-json-to-html.py
+++ api-docs/openapi-json-to-html.py
@@ -2,10 +2,10 @@
 
 HELP = """
 Usage:
-    python openapi-json-to-html.py -i <inputfile> -t <title> -o <outputfile>
+  python openapi-json-to-html.py -i <inputfile> -t <title> -o <outputfile>
 
 Example:
-    python openapi-json-to-html.py -i openapi.json -t "Nuveo Code Challenge" -o index.html
+  python openapi-json-to-html.py -i openapi.json -t "Nuveo Code Challenge" -o index.html
 """
 
 import sys, getopt, json
